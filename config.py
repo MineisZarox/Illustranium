2c0ce51085401c239ca20081f63657445e389561
Zarox <85471432+MineisZarox@users.noreply.github.com>
2023-03-12 12:45:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+class chivar(object):
+    API_ID = 0 
+    API_HASH = ""
+    BOT_TOKEN = ""
+    BOT_USERNAME = ""
+    OWNER_ID = 0
+    SUDO_IDS = "0 0"
+    LOGGER_GRP = LOG_GRP = -0
+    TOKEN = ""
