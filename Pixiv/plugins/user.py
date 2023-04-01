46ffac34a5ed0198e18cbf1dc40200d9a01a5089
Zarox <85471432+MineisZarox@users.noreply.github.com>
2023-04-01 22:29:45 +0530

    Update user.py

--- Pixiv/plugins/user.py
+++ Pixiv/plugins/user.py
@@ -4,56 +4,9 @@ from bs4 import BeautifulSoup
 from .. import pixiv, pxv, Vars
 from telethon import events, Button
 from telethon.events import CallbackQuery
-from .query import queryResults, seadict as userdict
 
-sudos = list(map(int, (Vars.SUDO_IDS).split(" ")))
-offlist = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600, 630, 660, 690, 720, 750, 780, 810, 840, 870, 900, 930, 960, 990, 1020, 1050, 1080, 1110, 1140, 1170, 1200, 1230, 1260, 1290, 1320, 1350, 1380, 1410, 1440, 1470, 1500, 1530, 1560, 1590, 1620, 1650, 1680, 1710, 1740, 1770, 1800, 1830, 1860, 1890, 1920, 1950, 1980, 2010, 2040, 2070, 2100, 2130, 2160, 2190, 2220, 2250, 2280, 2310, 2340, 2370, 2400, 2430, 2460, 2490, 2520, 2550, 2580, 2610, 2640, 2670, 2700, 2730, 2760, 2790, 2820, 2850, 2880, 2910, 2940, 2970]
-
-
-def ogiMas(url):
-    url = url.replace('original', 'master')
-    url = url.replace(url[-4:], f"_master1200.jpg")
-    return url
-
-async def usersResult(results, query, user_, offset=0, uc=0):
-    c = 0
-    cc = uc+1+offset
-    userdict[query] = []
-    for result in results['user_previews']:
-        rdict = {}
-        uid = result['user']['id']
-        details = await pxv.user_detail(uid)
-        rdict['id'] = details['user']['id']
-        rdict['name'] = details['user']['name']
-        rdict['uname'] = details['user']['account']
-        rdict['followers'] = details['profile']['total_follow_users']
-        rdict['illusn'] = details['profile']['total_illusts']
-        rdict['pimg'] = details['profile']['background_image_url'] if details['profile']['background_image_url'] else details['user']['profile_image_urls']['medium']
-        userdict[query].append(rdict)
-    pc = len(results['user_previews'])
-    if len(results['user_previews']) == 0: return "`0 results found of given query`"
-    c = uc
-    pc += offset
-    
-    url = f"https://www.pixiv.net/en/users/{userdict[query][c]['id']}"
-    caption = f"""**UserId** - {userdict[query][c]['id']}
-**Name** - [{userdict[query][c]['name']}]({url})
-**Username** - [{userdict[query][c]['uname']}]({url})
-**Followers** - {userdict[query][c]['followers']}
-**Illustrations** - {userdict[query][c]['illusn']}"""
-    img = userdict[query][c]['pimg']
-
-    buttons = [
-        [
-            Button.inline("Prev", data=f"bu_{c-1}_{user_}_{query}"),
-            Button.inline(f"{cc}/{pc}", data="cc"),
-            Button.inline("Next", data=f"nu_{c+1}_{user_}_{query}"),
-        ],
-        [
-            Button.inline("Illustraions", data=f"uq_{user_}_{userdict[query][c]['id']}")
-        ],
-    ]
-    return [img, caption, buttons]
+from . import *
+from . import seadict as userdict
 
 
 
